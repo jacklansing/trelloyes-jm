3e0c66a2916c4082b7797d55278f30695ef57398
Jack Lansing <john.a.lansing@gmail.com>
2020-03-02 14:15:12 -0500

    finished upp App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import List from './List';
+import STORE from '../STORE';
 
 function App() {
   return (
-    <div className="App">
-     
+    <main class="App">
+    <header class="App-header">
+      <h1>Trelloyes!</h1>
+    </header>
+    <div class="App-list">
+      {STORE.lists.map(list => <List 
+      key={list.id}
+      header={list.header}
+      cards={list.cardIds.map(card => STORE.allCards[card])}
+      />)}
     </div>
+    </main>
   );
 }
 
